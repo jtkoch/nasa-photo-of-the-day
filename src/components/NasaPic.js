18d2438c948dd992297ff01ef56ce88235754d48
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2019-11-19 21:59:51 -0600

    finished with styling

--- src/components/NasaPic.js
+++ src/components/NasaPic.js
@@ -3,7 +3,7 @@ import React from "react";
 const NasaPic = (props) => {
   return(
       <div className = "nasaPicture" key = {props.date}>
-          <img src= {props.url} alt="space" />
+          <img className = "Photo" src= {props.url} alt="space" />
           <h1>{props.title}</h1>
           <p>{props.explanation}</p>
       </div>
