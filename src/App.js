6f0b698e9735d9560700c17b500b2448d22b2e02
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2019-11-19 21:43:05 -0600

    all content is on page, Still need to style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
+import NasaPic from "./components/NasaPic";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://api.nasa.gov/planetary/apod?api_key=GAZaFR3y21FBeRsNzHdFwYGrzkqgzhNktUAUG7ff')
+      .then(response => {
+        console.log(response.data);
+        setNasaData(response.data);
+      })
+      .catch(error => {
+        console.log("There was no data!", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <header><h1>NASA PHOTO OF THE DAY</h1></header>
+      <NasaPic date={nasaData.date} title={nasaData.title} explanation={nasaData.explanation} url={nasaData.hdurl} />
     </div>
   );
 }
---
+++ src/components/NasaPic.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+const NasaPic = (props) => {
+  return(
+      <div className = "nasaPicture" key = {props.date}>
+          <img src= {props.url} alt="space" />
+          <h1>{props.title}</h1>
+          <p>{props.explanation}</p>
+      </div>
+  )
+}
+export default NasaPic; 
