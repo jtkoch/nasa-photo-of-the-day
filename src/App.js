90e3da4f012afde3a888c8ad46a2b9718d1ee582
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2020-01-07 15:06:56 -0600

    redoing this project from scratch - initial commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
-import React, {useState, useEffect} from "react";
-import axios from "axios";
+import React from "react";
 import "./App.css";
-import NasaPic from "./components/NasaPic";
-import styled from "styled-components";
-
-const ApodTitle = styled.div ` 
-  text-align: center;
-  background-color: black;
-  color: maroon;
-  font-size: 70px;
-  font-family: 'Odibee Sans', cursive;
-`;
 
 function App() {
-  const [nasaData, setNasaData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=GAZaFR3y21FBeRsNzHdFwYGrzkqgzhNktUAUG7ff')
-      .then(response => {
-        console.log(response.data);
-        setNasaData(response.data);
-      })
-      .catch(error => {
-        console.log("There was no data!", error);
-      });
-  }, []);
-
   return (
-    <nasaTitle className="App">
-      <ApodTitle>NASA PHOTO OF THE DAY</ApodTitle>
-      <NasaPic date={nasaData.date} title={nasaData.title} explanation={nasaData.explanation} url={nasaData.hdurl} />
-    </nasaTitle>
+    <div className="App">
+      <p>
+        Read through the instructions in the README.md file to build your NASA
+        app! Have fun 🚀!
+      </p>
+    </div>
   );
 }
 
--- src/components/NasaPic.js
+++ src/components/NasaPic.js
@@ -1,41 +1,5 @@
 import React from "react"; 
-import styled from "styled-components";
-
-
-const PhotoDiv = styled.div `
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background-color: black;
-`;
-
-const TextContainer = styled.div`
-  color: maroon;
-  font-family: 'Odibee Sans', cursive;
-`;
-
-const PhotoTitle = styled.h1`
-  font-size: 50px;
-  text-align: center;
-`;
-
-const PhotoImage = styled.img`
-  border-radius: 5%; 
-  width: 500px;
-  height: 500px;
-  margin: 20px;
-`;
-
-const PhotoExplanation = styled.p`
-  width: 90%;
-  text-align: center;
-  margin: 50px;
-  font-size: 20px;
-`;  
-
-
-
+// import styled from "styled-components";
 
 const NasaPic = props => {
   return (
@@ -51,3 +15,36 @@ const NasaPic = props => {
 };  
 
 export default NasaPic;
+
+
+// const PhotoDiv = styled.div `
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: black;
+// `;
+
+// const TextContainer = styled.div`
+//   color: maroon;
+//   font-family: 'Odibee Sans', cursive;
+// `;
+
+// const PhotoTitle = styled.h1`
+//   font-size: 50px;
+//   text-align: center;
+// `;
+
+// const PhotoImage = styled.img`
+//   border-radius: 5%; 
+//   width: 500px;
+//   height: 500px;
+//   margin: 20px;
+// `;
+
+// const PhotoExplanation = styled.p`
+//   width: 90%;
+//   text-align: center;
+//   margin: 50px;
+//   font-size: 20px;
+// `;  
