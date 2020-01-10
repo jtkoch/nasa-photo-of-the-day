3b4333058724f5923b18e80154c5e6c92c46b55e
Jensen Koch <pdpdrums12@Jensens-MacBook-Pro.local>
2020-01-09 20:18:33 -0600

    MVP part 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import "./App.css";
 import NasaPic from "./components/NasaPic";
+import styled from "styled-components";
+
+const ApodTitle = styled.div ` 
+  text-align: center;
+  background-color: black;
+  color: maroon;
+  font-size: 70px;
+  font-family: 'Odibee Sans', cursive;
+`;
 
 function App() {
   const [nasaData, setNasaData] = useState([]);
-
   useEffect(() => {
     axios
       .get('https://api.nasa.gov/planetary/apod?api_key=GAZaFR3y21FBeRsNzHdFwYGrzkqgzhNktUAUG7ff')
@@ -14,17 +22,16 @@ function App() {
         setNasaData(response.data);
       })
       .catch(error => {
-        console.log("No data to be found!", error);
+        console.log("There was no data!", error);
       });
-}, []);
+  }, []);
 
-return (
-  <div className="App">
-    <header><h1>NASA PHOTO OF THE DAY</h1></header>
-    <NasaPic date={nasaData.date} title={nasaData.title} explanation={nasaData.explanation} url={nasaData.hdurl} />
-  </div>
+  return (
+    <nasaTitle className="App">
+      <ApodTitle>NASA PHOTO OF THE DAY</ApodTitle>
+      <NasaPic date={nasaData.date} title={nasaData.title} explanation={nasaData.explanation} url={nasaData.hdurl} />
+    </nasaTitle>
   );
-}  
-
+}
 
 export default App;
